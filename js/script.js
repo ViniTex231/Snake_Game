6aaaa610a81c06e41e32c755ba81442a32f48f05
ViniTex231 <viniteixeira2828@gmail.com>
2023-07-12 14:21:14 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var blockSize = 25
+var rows = 20
+var cols = 20
+var board
+var context
+
+var snakeX = blockSize * 5
+var snakeY = blockSize * 5
+
+var velocityX = 0
+var velocityY = 0
+
+var foodX
+var foodY
+
+window.onload = function(){
+    board = document.getElementById('board')
+    board.height = rows * blockSize
+    board.width = cols * blockSize
+    context = board.getContext("2d")
+
+    placeFood()
+    document.addEventListener('keyup', changeDirection)
+    update()
+}
+
+function update() {
+    context.fillStyle = 'black'
+    context.fillRect(0, 0, board.width, board.height)
+    context.fillStyle = 'lime'
+    context.fillRect(snakeX, snakeY, blockSize, blockSize)
+    context.fillStyle='red'
+    context.fillRect(foodX, foodY, blockSize, blockSize)
+}
+
+function changeDirection(){
+    if(e.code == "ArrowUp"){
+        velocityX = 0
+        velocityY = -1
+
+    }
+}
+
+function placeFood(){
+    foodX = Math.floor(Math.random() * cols) * blockSize
+    foodY = Math.floor(Math.random() * rows) * blockSize
+}
